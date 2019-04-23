a505b8a12305bdb1d27e1107e69ac03a4e4c0818
MuhammadNurAshiddiqi <48676131+MuhammadNurAshiddiqi@users.noreply.github.com>
2019-04-23 22:17:51 +0700

    Create expression.cpp

---
+++ expression.cpp
@@ -0,0 +1,225 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+using namespace std;
+int getlevel(char input){
+	if(input == '^')
+		return 3;
+	else if(input == '*' || input == '/')
+		return 2;
+	else if(input == '+' || input == '-')
+		return 1;
+	else if(input == '(' || input == ')')
+		return 0;
+	else
+		return -1;
+}
+
+struct stack{
+	int STACK[100000];
+	char Stack[100000];
+	int top;
+};
+
+class Conv{
+private:
+	stack list;
+
+public:
+	void init(){
+		list.top = -1;
+	}
+
+	void push(char input){
+		list.top++;
+		list.STACK[list.top] = input;
+	}
+
+	void pop(){
+		list.top--;
+	}
+
+	char Top(){
+		return list.STACK[list.top];
+	}
+
+	bool isEmpty(){
+		if(list.top <= -1)
+			return true;
+		else
+			return false;
+	}
+}; Conv SC;
+
+class Eval{
+private:
+	stack Data;
+
+public:
+	void push(int input){
+		Data.top++;
+		Data.Stack[Data.top] = input;
+	}
+
+	void pop(){
+		Data.top--;
+	}
+
+	int Top(){
+		return Data.Stack[Data.top];
+	}
+}; Eval SE;
+
+
+bool isOperand(char input);
+bool isOperator(char input);
+int getlevel(char input);
+bool precedence(char input1, char input2);
+string InfixtoPostfix(string input);
+
+double Operation(int op1, char c, int op2);
+bool Operand(char input);
+int Evaluate(string input);
+int pow(int number, int exp);
+
+bool isOperand(char input){
+	int c = (int)input;
+	if(c >= 48 && c <= 57 || c >= 65 && c <= 90 || c >= 97 && c <= 122)//
+		return true;
+	else
+		return false;
+}
+
+bool isOperator(char input){
+	int c = (int)input;
+	if(c == 94 || c >= 42 && c <= 43 || c == 45 || c == 47)//
+		return true;
+	else
+		return false;
+}
+
+
+bool precedence(char input1, char input2){
+	int c1 = getlevel(input1);
+	int c2 = getlevel(input2);
+
+	if(c1 <= c2)
+		return true;
+	else
+		return false;
+}
+
+string InfixtoPostfix(string input){
+	SC.init();
+	int i=0;
+	string P = "";
+	while(input[i] != '\0'){
+		if(isOperand(input[i])){
+			P += input[i];
+		}
+		if(input[i] == '('){
+			SC.push(input[i]);
+		}
+		if(input[i] == ')'){
+			while(!SC.isEmpty() && SC.Top() != '('){
+				P = P + " " + SC.Top(); SC.pop();
+			}
+			SC.pop();
+		}
+		if(isOperator(input[i])){
+			if(SC.isEmpty() || SC.Top() == '('){
+				SC.push(input[i]);
+			}else{
+				while(!SC.isEmpty() && SC.Top() != '(' && precedence(input[i], SC.Top())){
+					P = P + " " + SC.Top(); SC.pop();
+				}
+				SC.push(input[i]);
+			}
+		}
+		if(isOperator(input[i]))
+			P += " ";
+		i++;
+	}
+	while(!SC.isEmpty()){
+		P = P + " " + SC.Top(); SC.pop();
+	}
+	return P;
+}
+
+
+int pow(int number, int exp){
+	for(int i=1; i<exp; i++)
+		number *= number;
+	return number;
+}
+
+bool Operand(char input){
+	int c = (int)input;
+	if(c >= 48 && c <= 57)
+		return true;
+	else if(c >= 65 && c <= 90 || c >= 97 && c <= 122){
+		cout << "\nNot Evaluated" << endl;
+		cin.get();
+		exit(0);
+	}
+	else
+		return false;
+}
+
+double Operation(int op1, char c, int op2){
+	if(c == '^')
+		return (pow(op1,op2));
+	else if(c == '*')
+		return (op1 * op2);
+	else if(c == '/')
+		return (op1 / op2);
+	else if(c == '+')
+		return (op1 + op2);
+	else if(c == '-')
+		return (op1 - op2);
+	else return 0;
+}
+
+int Evaluate(string input){
+	int i = 0;
+	while(input[i] != '\0'){
+		if(Operand(input[i])){
+			int operand = 0;
+			while(input[i] != '\0' && Operand(input[i])){
+				operand = (operand * 10) + (input[i] - 48);
+				i++;
+			}
+			SE.push(operand);
+		}
+		if(isOperator(input[i])){
+			int A = SE.Top(); SE.pop();
+			int B = SE.Top(); SE.pop();
+			int result = Operation(B,input[i],A);
+			SE.push(result);
+		}
+		i++;
+	}
+	return SE.Top();
+}
+int main(){
+
+	string I;
+	cout<<"\t "<<endl;
+	cout<<"||=====================================================||"<<endl;
+	cout<<"||              EVALUATE AN EXPRESSION                 ||"<<endl;
+	cout<<"||               DEVELOPED BY BulinMcD                 ||"<<endl;
+	cout<<"||=====================================================||"<<endl;
+	cout<<"||My Github = https://github.com/MuhammadNurAshiddiqi  ||"<<endl;
+	cout<<"||=====================================================||"<<endl<<endl;
+	cout<<"||Enter Infix Expression = ";
+	getline(cin,I);
+	string P = InfixtoPostfix(I);
+	cout<<"||Infix Your Enter = "<< I;
+	cout<<"\n||Postfix of Infix = "<< P;
+	int result = Evaluate(P);
+	cout<<"\n||Evaluate Expression = "<< result;
+
+	cin.get();
+	return 0;
+}
